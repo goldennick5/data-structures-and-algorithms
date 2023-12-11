0f0609d43ac4033c4e9423579c10a74ff55ab93c
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-11 15:20:55 +0600

    additional

--- Recursion.java
+++ Recursion.java
@@ -4,9 +4,10 @@ public class Recursion {
     long nFactorial = recursion.nFactorial(4);
     System.out.println(nFactorial);
     int Fibonacci = recursion.Fibonacci(4);
-    System.err.println(Fibonacci);
+    System.out.println(Fibonacci);
+    recursion.printFibonacciSequence(9);
   }
-  
+
   // 1.0 Factorial Problem
   public long nFactorial(int n) {
     if(n < 0) return -1;
@@ -19,6 +20,16 @@ public class Recursion {
   // ---> 2 * nFactorial(1) => 2
 
   // 2.0 Fibonacci Problem
+  public void printFibonacciSequence(int n) {
+    if(n < 0) {
+      System.out.println("invalid input");
+      return;
+    };
+    for(int i = 0; i <= n; i++) {
+      System.out.print(Fibonacci(i) + " ");
+    }
+  }
+
   public int Fibonacci(int n) {
     if(n < 0) return -1;
     if(n == 0 || n == 1) return n;
@@ -28,5 +39,7 @@ public class Recursion {
     //                 2  Fibonacci(3)         <----->       Fibonacci(2)  1
     //      1 + 1  Fibonacci(2) + Fibonacci(1) <-----> Fibonacci(1) + Fibonacci(0)  1
     //   1  Fibonacci(1) + Fibonacci(0)
+
+    //test
   }
 }
