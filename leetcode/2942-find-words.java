25b1525c2bb90654dd34f597229e75e42d05012a
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-27 23:05:30 +0600

    redone task

--- leetcode/2942-find-words.java
+++ leetcode/2942-find-words.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
-//dumbest solution that comes in my mind
 class Solution {
-    public List<Integer> findWordsContaining(String[] words, char x) {
+    //dumbest solution that firstly comes in my mind
+    public List<Integer> findWordsContainingFirst(String[] words, char x) {
         //to store unique values
         HashSet<Integer> res = new HashSet<Integer>();
         ArrayList<Integer> finalRes = new ArrayList<Integer>();
@@ -22,4 +22,21 @@ class Solution {
         finalRes.addAll(res);
         return finalRes;
     }
+
+    
+    //better solution not using set and just breaking after add number and pursue to the next
+    public List<Integer> findWordsContainingSecond(String[] words, char x) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        for(int i = 0; i < words.length; i++) {
+            for(int j = 0; j < words[i].length(); j++) {
+                if(words[i].charAt(j) == x) {
+                    res.add(i);
+                    break;
+                }
+            }
+        }
+        return res;
+    }
 }
+
+
