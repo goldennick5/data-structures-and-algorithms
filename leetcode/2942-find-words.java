befd276c62516a898e3260584d237ed84136f4c7
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-27 23:13:33 +0600

    done task

--- leetcode/2942-find-words.java
+++ leetcode/2942-find-words.java
@@ -5,38 +5,53 @@ import java.util.HashSet;
 import java.util.List;
 
 class Solution {
-    //dumbest solution that firstly comes in my mind
-    public List<Integer> findWordsContainingFirst(String[] words, char x) {
-        //to store unique values
-        HashSet<Integer> res = new HashSet<Integer>();
-        ArrayList<Integer> finalRes = new ArrayList<Integer>();
-        for(int i = 0; i < words.length; i++) {
-            for(int j = 0; j < words[i].length(); j++) {
-                //if char equal to target char, i am adding to set
-                if(words[i].charAt(j) == x) {
-                    res.add(i);
-                }
-            }
+  // dumbest solution that firstly comes in my mind
+  public List<Integer> findWordsContainingFirst(String[] words, char x) {
+    // to store unique values
+    HashSet<Integer> res = new HashSet<Integer>();
+    ArrayList<Integer> finalRes = new ArrayList<Integer>();
+    for (int i = 0; i < words.length; i++) {
+      for (int j = 0; j < words[i].length(); j++) {
+        // if char equal to target char, i am adding to set
+        if (words[i].charAt(j) == x) {
+          res.add(i);
         }
-        //copying values from set to list, because func is returning list
-        finalRes.addAll(res);
-        return finalRes;
+      }
     }
+    // copying values from set to list, because func is returning list
+    finalRes.addAll(res);
+    return finalRes;
+  }
 
-    
-    //better solution not using set and just breaking after add number and pursue to the next
-    public List<Integer> findWordsContainingSecond(String[] words, char x) {
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        for(int i = 0; i < words.length; i++) {
-            for(int j = 0; j < words[i].length(); j++) {
-                if(words[i].charAt(j) == x) {
-                    res.add(i);
-                    break;
-                }
-            }
+  // better solution not using set and just breaking after add number and pursue
+  // to the next
+  public List<Integer> findWordsContainingSecond(String[] words, char x) {
+    ArrayList<Integer> res = new ArrayList<Integer>();
+    for (int i = 0; i < words.length; i++) {
+      for (int j = 0; j < words[i].length(); j++) {
+        if (words[i].charAt(j) == x) {
+          res.add(i);
+          break;
         }
-        return res;
+      }
     }
+    return res;
+  }
+
+  // i also can do like this
+  public List<Integer> findWordsContainingThird(String[] words, char x) {
+    ArrayList<Integer> res = new ArrayList<Integer>();
+    for (int i = 0; i < words.length; i++) {
+      //getting char j looping using : and converting String to char array
+      for (char j : words[i].toCharArray()) {
+        if (j == x) {
+          res.add(i);
+          break;
+        }
+      }
+    }
+    return res;
+  }
+
+  
 }
-
-
