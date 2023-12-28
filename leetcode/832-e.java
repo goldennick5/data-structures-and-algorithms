8fb6032d1482619062890994f0d81f4fccb155b3
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-28 17:53:03 +0600

    done task

--- leetcode/832-e.java
+++ leetcode/832-e.java
@@ -5,7 +5,6 @@ class Solution {
     public int[][] flipAndInvertImage(int[][] image) {
         int start;
         int end;
-        //firstly, i am flipping an array
         for (int i = 0; i < image.length; i++) {
             start = 0;
             end = image[i].length - 1;
@@ -17,16 +16,9 @@ class Solution {
                 end--;
             }
         }
-        //secondly, I just invert it
         for(int i = 0; i < image.length; i++) {
             for(int j = 0; j < image[i].length; j++) {
-                int num = image[i][j];
-                if(num == 0) {
-                    image[i][j] = 1;
-                }
-                if(num == 1) {
-                    image[i][j] = 0;
-                }
+                image[i][j] = (image[i][j] == 0) ? 1 : 0;
             }
         }
         return image;
