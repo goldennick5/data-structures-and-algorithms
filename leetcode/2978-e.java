64bcf77d211bb93e5bd4db407b7cb8f7ddac71d2
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-28 11:58:00 +0600

    done task

---
+++ leetcode/2978-e.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int counter = 0;
+        for(int i = 0; i < hours.length; i++) {
+            if(hours[i] >= target) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
+
+
+// Example 1:
+
+// Input: hours = [0,1,2,3,4], target = 2
+// Output: 3
+// Explanation: The company wants each employee to work for at least 2 hours.
+// - Employee 0 worked for 0 hours and didn't meet the target.
+// - Employee 1 worked for 1 hours and didn't meet the target.
+// - Employee 2 worked for 2 hours and met the target.
+// - Employee 3 worked for 3 hours and met the target.
+// - Employee 4 worked for 4 hours and met the target.
+// There are 3 employees who met the target.
