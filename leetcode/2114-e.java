2f8bf1beb2817935a10990e8f28660fe1b5ddcac
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2023-12-31 21:54:56 +0600

    done task

--- leetcode/2114-e.java
+++ leetcode/2114-e.java
@@ -1,18 +1,30 @@
 package leetcode;
 
 class Solution {
-    public int mostWordsFound(String[] sentences) {
-        int[] res = new int[sentences.length];
-        int max = 0;
-        for(int i = 0; i < sentences.length; i++) {
-            String[] wordArr = sentences[i].trim().split(" ");
-            res[i] = wordArr.length;
-        }
-        for(int i = 0; i < res.length; i++) {
-            if(max < res[i]) {
-                max = res[i];
-            }
-        }
-        return max;
+  // ultra super mega gega solution
+  public int mostWordsFound(String[] sentences) {
+    int[] res = new int[sentences.length];
+    int max = 0;
+    for (int i = 0; i < sentences.length; i++) {
+      String[] wordArr = sentences[i].trim().split(" ");
+      res[i] = wordArr.length;
     }
+    for (int i = 0; i < res.length; i++) {
+      if (max < res[i]) {
+        max = res[i];
+      }
+    }
+    return max;
+  }
+
+  // more better solution
+  public int mostWordsFoundSec(String[] sentences) {
+    int max = 0;
+    for (String s : sentences) {
+      int wordLen = s.trim().split(" ").length;
+      if (max < wordLen)
+        max = wordLen;
+    }
+    return max;
+  }
 }
