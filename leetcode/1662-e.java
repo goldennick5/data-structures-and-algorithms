15dc3b7f2ed167bea73d900f747a9bedbf683aca
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-03 11:03:24 +0600

    done task

--- leetcode/1662-e.java
+++ leetcode/1662-e.java
@@ -1,20 +1,15 @@
 package leetcode;
 
 class Solution {
-  //bruit force solution
   public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
     StringBuilder str_first = new StringBuilder();
     StringBuilder str_second = new StringBuilder();
     for (int i = 0; i < word1.length; i++) {
-      String word = word1[i];
-      str_first.append(word);
+      str_first.append(word1[i]);
     }
     for (int i = 0; i < word2.length; i++) {
-      String word = word2[i];
-      str_second.append(word);
+      str_second.append(word2[i]);
     }
-    if (str_first.toString().equals(str_second.toString()))
-      return true;
-    return false;
+    return str_first.compareTo(str_second) == 0;
   }
 }
