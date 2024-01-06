2d142158186eccd9b569e59b21888fe8fee26375
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-06 21:18:45 +0600

    done task

--- leetcode/303-sum-range.java
+++ leetcode/303-sum-range.java
@@ -4,14 +4,14 @@ class NumArray {
     int[] prefixSum;
 
     public NumArray(int[] nums) {
-        for(int i = 1; i < nums.length; i++) {
-            nums[i] += nums[i - 1];
-        }
         this.prefixSum = nums;
+        for(int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] += prefixSum[i - 1];
+        }
     }
     
     public int sumRange(int left, int right) {
-        if(left == 0) return prefixSum[right];
+        if(left == 0) return this.prefixSum[right];
         return prefixSum[right] - prefixSum[left - 1];
     }
 
