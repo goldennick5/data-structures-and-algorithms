7e74d7ecd5953bfc3ee2b560a8b688405384a8a9
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-07 19:36:48 +0600

    fix

--- leetcode/1-two-sum.java
+++ leetcode/1-two-sum.java
@@ -2,15 +2,13 @@ package leetcode;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] res = new int[2];
         for(int i = 0; i <= nums.length - 1; i++) {
             for(int j = i + 1; j <= nums.length - 1; j++) {
                 if(nums[i] + nums[j] == target) {
-                    res[0] = i;
-                    res[1] = j;
+                    return new int[] {i, j};
                 }
             }
         }
-        return res;
+        return new int[] {};
     }
 }
