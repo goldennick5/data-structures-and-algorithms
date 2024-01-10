ebdc6fc9133a6029157f810af79b2592128acfc9
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-10 22:18:48 +0600

    done task

--- leetcode/1816-e.java
+++ leetcode/1816-e.java
@@ -10,3 +10,10 @@ class Solution {
     return String.join(" ", res);
   }
 }
+
+
+//js solution easy peasy lemon sq
+
+// var truncateSentence = function(s, k) {
+//     return s.split(" ").slice(0, k).join(" ");
+// };
--- leetcode/535-m.java
+++ leetcode/535-m.java
@@ -2,12 +2,12 @@ public class Codec {
 
   // Encodes a URL to a shortened URL.
   public String encode(String longUrl) {
-      return longUrl;
+    return longUrl;
   }
 
   // Decodes a shortened URL to its original URL.
   public String decode(String shortUrl) {
-      return shortUrl;
+    return shortUrl;
   }
 }
 
