7eb58d72100a4df304317d552a1dc70f573a26b9
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-12 14:55:31 +0600

    redone task

--- leetcode/1282-m.java
+++ leetcode/1282-m.java
@@ -3,11 +3,10 @@ package leetcode;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 class Solution {
   public List<List<Integer>> groupThePeople(int[] groupSizes) {
-    Map<Integer, List<Integer>> map = new HashMap<>();
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
     List<List<Integer>> res = new ArrayList<>();
 
     for (int i = 0; i < groupSizes.length; i++) {
@@ -18,7 +17,7 @@ class Solution {
       map.get(size).add(i);
       if (map.get(size).size() == size) {
         res.add(map.get(size));
-        map.put(size, new ArrayList<>());
+        map.remove(size);
       }
     }
     return res;
