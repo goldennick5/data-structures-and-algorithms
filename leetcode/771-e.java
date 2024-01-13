51150097c4fccea15abeef26192b8fb5880c7851
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-13 21:36:33 +0600

    fix

--- leetcode/771-e.java
+++ leetcode/771-e.java
@@ -2,11 +2,11 @@ package leetcode;
 
 class Solution {
   public int numJewelsInStones(String jewels, String stones) {
-    int counter = 0;
+    int res = 0;
     for (char stone : stones.toCharArray()) {
-      if (jewels.indexOf(stone) > -1)
-        counter++;
+      if (jewels.indexOf(stone) != -1)
+        res++;
     }
-    return counter;
+    return res;
   }
 }
