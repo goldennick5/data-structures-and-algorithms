6b096a6cca4ac4b5b44acdaf1eb1a5bc3903850b
Tokhtar Aubakirov <toxaaubakirovv2002@gmail.com>
2024-01-27 23:23:43 +0600

    redone

--- leetcode/13-e.java
+++ leetcode/13-e.java
@@ -25,3 +25,29 @@ class Solution {
     return res;
   }
 }
+
+
+// class Solution {
+//     public int romanToInt(String s) {
+//         Map<Character, Integer> m = new HashMap<>();
+//         int res = 0;
+
+//         m.put('I', 1);
+//         m.put('V', 5);
+//         m.put('X', 10);
+//         m.put('L', 50);
+//         m.put('C', 100);
+//         m.put('D', 500);
+//         m.put('M', 1000);
+
+//         for (int i = 0; i < s.length(); i++) {
+//             if (i < s.length() - 1 && m.get(s.charAt(i)) < m.get(s.charAt(i + 1))) {
+//                 res -= m.get(s.charAt(i));
+//             } else {
+//                 res += m.get(s.charAt(i));
+//             }
+//         }
+
+//         return res;
+//     }
+// }
